7bbabbbd42aa9f07e953d3a92e65ee3e340e8081
MidnightP <heijden.pvd@gmail.com>
2018-01-28 23:18:20 +0100

    initial

--- static/asanaGuessingGame.js
+++ static/asanaGuessingGame.js
@@ -1,107 +1,82 @@
 // selectors
-var squares = document.getElementsByClassName("square");
+var allSquares = document.getElementsByClassName("square");
+var currentSquares = allSquares
+var hiddenSquares = document.getElementsByClassName("square hidden");
 var asanaDisplay = document.getElementById("asanaDisplay");
 var messageDisplay = document.querySelector("#message");
 var resetButton = document.querySelector("#reset");
+var h1 = document.querySelector("h1");
+var easyBtn = document.querySelector("#easyBtn");
+var hardBtn = document.querySelector("#hardBtn");
 
-var usedIndices = []
-var numSquares = squares.length;
-var randomAsanas = generateAsanasArr();
-var chosenAsana = pickAsana(randomAsanas);
+// variables
+var usedIndices = [];
+var ended = false
+var numSquares = currentSquares.length;
+var hardNumSquares = allSquares.length;
+var easyNumSquares = 3;
+var randomAsanas = [];
+var chosenAsana = {};
 
+// functions
+function reset() {
+    usedIndices = []
+    ended = false;
+    randomAsanas = [];
+    chosenAsana = {};
 
-// var winnerAsana = generateRandomAsana();
-
-//var h1 = document.querySelector("h1");
-//var easyBtn = document.querySelector("#easyBtn");
-//var hardBtn = document.querySelector("#hardBtn");
-
-//easyBtn.addEventListener("click", function(){
-//  easyBtn.classList.add("selected");
-//  hardBtn.classList.remove("selected");
-////  numSquares = generateRandomAsanas(numSquares);
-//  numSquares = randomAsana(numSquares);
-////  asanas = generateRandomAsanas(numSquares);
-//  asanas = randomAsana(numSquares);
-//  pickedAsana = pickAsana();
-//  asanaDisplay.textContent = pickedAsana;
-//    for(var i = 0; i < squares.length; i++){
-//        if(asanas[i]){
-//            squares[i].style.background = asanas[i];
-//        } else {
-//            squares[i].style.display = "none";
-//        }
-//    }
-//});
-
-//hardBtn.addEventListener("click", function(){
-//  easyBtn.classList.remove("selected");
-//  hardBtn.classList.add("selected");
-//  numSquares = 6;
-//  asanas = generateRandomAsanas(numSquares);
-//  pickedAsana = pickAsana();
-//  asanaDisplay.textContent = pickedAsana;
-//    for(var i = 0; i < squares.length; i++){
-//            squares[i].style.background = asanas[i];
-//            squares[i].style.display = "block";
-//        }
-//});
-
-
-resetButton.addEventListener("click", function () {
     // generate all new asanas
-    // asanas = generateRandomAsanas(numSquares);
-    // generate all new asanas
-    asanas = randomAsana(numSquares);
+    generateAsanasArr();
+
     // pick a new random asana from array
-    pickedAsana = pickAsana();
-    // change asana display to match picked asana
-    asanaDisplay.textContent = pickedAsana;
-    this.textContent = "New Asanas";
+    pickAsana(randomAsanas);
+
+    // its name in the title
+    asanaDisplay.textContent = chosenAsana.name;
+
+    resetButton.textContent = "New Asanas";
     messageDisplay.textContent = "";
-    // change asanas in squares on page
-    for (var i = 0; i < squares.length; i++) {
-        squares[i].style.backgroundColor = asanas[i];
-    }
     h1.style.backgroundColor = "steelblue";
-})
 
-// Place asanas in sqares
-for (var i = 0; i < numSquares; i++) {
+    // Place asanas in squares
+    for (var i = 0; i < numSquares; i++) {
+        var square = currentSquares[i]
+        var asana = randomAsanas[i]
 
-    var square = squares[i]
+        // attach asana image to square
+        square.style.backgroundImage = `url('${asana.image}')`
 
-    //add inital asana to squares
-    square.style.backgroundImage = `url('${randomAsanas[i].image}')`
-
-    var asana = randomAsanas[i]
-
-    setOnClickListener(asana, square)
+        setOnClick(asana, square)
+    }
 }
 
-function setOnClickListener(asana, square) {
-    //add click listeners to squares
-    square.addEventListener("click", function () {
+function setOnClick(asana, square) {
+    square.clicked = false;
+
+    // 3add click listeners to squares
+    square.onclick = function () {
+
+        if (square.clicked || ended) return
+
+        square.clicked = true
 
         // compare asana to picked asana
         if (asana.name === chosenAsana.name) {
+            // won!
             messageDisplay.textContent = "Got it!";
             resetButton.textContent = "Play again?";
             changeAsanas(asana);
-            h1.style.backgroundImage = `url('${asana.image}')`;
+            h1.style.backgroundColor = "gold"
+
+            ended = true
+
         } else {
-            this.style.backgroundImage = "";
+            // try again!
+            square.style.backgroundImage = "";
             messageDisplay.textContent = "Try Again!";
         }
-    });
+    };
 }
-
-
-// var randomAsanas = generateAsanasArr(); // var creates asana array
-// var pickedAsana = pickAsana(randomAsanas);
-//console.log("randomAsanas", randomAsanas);
-//console.log("pickedAsana", pickedAsana);
-//console.log("foundAsana", findAsanaByName(pickedAsana))
 
 function generateNewRandomIndex() {
     var index = Math.floor(Math.random() * asanaPics.length);
@@ -127,33 +102,74 @@ function generateRandomAsana() {
 //Create a function to pick an asana from an array
 function pickAsana(asanas) {
     var random = Math.floor(Math.random() * numSquares);
-    return asanas[random];
+    chosenAsana = asanas[random];
 }
 
 //Create a function to create an array of asanas
 function generateAsanasArr() {
-    var asanas = []
     for (var i = 0; i < numSquares; i++) {
         //get random asana and push into array
-        asanas.push(generateRandomAsana())
+        randomAsanas.push(generateRandomAsana())
     }
-    return asanas
 }
-
-//finds row in table
-// function findAsanaByName(name) {
-//     function isRightName(asanaObj) {
-//         return asanaObj.name === name;
-//     }
-
-//     return asanaPics.find(isRightName)
-// }
 
 //Create a function to
 function changeAsanas(asana) {
     //loop through all squares
     for (var i = 0; i < numSquares; i++) {
         //change each square to match given asana
-        squares[i].style.backgroundImage = `url('${asana.image}')`;
+        currentSquares[i].style.backgroundImage = `url('${asana.image}')`;
     }
 }
+
+resetButton.onclick = reset
+
+hardBtn.onclick = function () {
+    hardBtn.classList.add("selected");
+    easyBtn.classList.remove("selected");
+
+    // set all squares to be the current squares
+    currentSquares = allSquares;
+
+    // set the new number of squares
+    numSquares = currentSquares.length;
+
+    for (var i = 0; i < numSquares; i++) {
+        allSquares[i].className = "square"
+    };
+
+    reset();
+};
+
+easyBtn.onclick = function () {
+    easyBtn.classList.add("selected");
+    hardBtn.classList.remove("selected");
+
+    // calculate the number of squares to be hidden in the html
+    var amountToBeRemoved = numSquares - easyNumSquares;
+
+    // set the new number of squares
+    numSquares = easyNumSquares;
+
+    for (var i = 0; i < amountToBeRemoved; i++) {
+        allSquares[2 + i].className = "square hidden"
+    };
+
+    currentSquares = [];
+
+    for (var i = 0; i < allSquares.length; i++) {
+        if (!allSquares[i].className.includes("hidden")) currentSquares.push(allSquares[i])
+    };
+
+    reset();
+};
+
+reset();
+
+// finds row in table
+// function findAsanaByName(name) {
+//     function isRightName(asanaObj) {
+//         return asanaObj.name === name;
+//     }
+//     return asanaPics.find(isRightName)
+// }
